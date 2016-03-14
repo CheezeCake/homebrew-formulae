d3ac34290e2599a87c95f2032aa16f6264d223f4
CheezeCake <emmanuel.nicolet@gmail.com>
2016-03-14 20:55:25 +0100

    fix url + use https

--- Formula/libircclient.rb
+++ Formula/libircclient.rb
@@ -1,7 +1,7 @@
 class Libircclient < Formula
   desc "Small but powerful library, which implements client-server IRC protocol"
   homepage "http://www.ulduzsoft.com/libircclient/"
-  url "http://downloads.sourceforge.net/sourceforge/libircclient/libirclient-1.8.tar.gz"
+  url "https://downloads.sourceforge.net/sourceforge/libircclient/libircclient-1.8.tar.gz"
   sha256 "2cf8523ac683588f2785b08159a2df3e4d15939ee655c0024aa86334149de8f6"
 
   def install
